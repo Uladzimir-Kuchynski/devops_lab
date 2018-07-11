4927e8d2bd7453bb0e57a2de2a7e2fdad20b05b8
student <student@EPBYMINW2629.minsk.epam.com>
2018-07-11 17:32:22 +0300

    “homework2_task_01_and_03”

--- Task_01.py
+++ Task_01.py
@@ -1,8 +1,8 @@
 students = {}
 score_01 = []
 b = []
-for i in range(int(input("Students \n"))):
-    name = input("Enter_Name_Of_Students \n")
+for i in range(int(input("Number_Of_Students \n"))):
+    name = input("Enter_Name_Of_Student \n")
     score = float(input("Enter_Score \n"))
     students[name] = score
     score_01.append(score)
@@ -12,4 +12,4 @@ for name, score in students.items():
     if score == a:
         b.append(name)
 
-print(sorted (b))
+print(sorted(b))
--- Task_03.py
+++ Task_03.py
@@ -1,13 +1,13 @@
 n = int(input("n="))
 lst = []
-for i in range(2, n+1):
+for i in range(2, n + 1):
     for j in range(2, i):
         if i % j == 0:
             # если делитель найден, число не простое.
             break
     else:
         lst.append(i)
-print (lst)
+print(lst)
 
 
 myString = ''.join(str(x) for x in lst)
@@ -16,14 +16,9 @@ print(myString)
 
 m = int(input("m=\n"))
 
-l = list (map(int,input().split()))
+l = list(map(int, input().split()))
 
 for i in l:
     print(myString[i - 1])
 
-#    print(myString[i-1])
 #    '\n'.join(t)
-
-
-
-
